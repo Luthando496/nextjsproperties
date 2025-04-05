d6040d29367e51ffbe8c63aaa46735bc35f20719
Luthando496 <luthandodidiza197@gmail.com>
2025-04-05 18:49:52 +0200

    image change v4

--- app/actions/newPost.js
+++ app/actions/newPost.js
@@ -4,34 +4,14 @@ import { supabase } from "@/utils/connectDB";
 
 export const newPost = async (myData) => {
   try {
-    const fileName = `${Date.now()}_${myData.post_image.name}`;
-
-    const { data: uploadData, error: uploadError } = await supabase.storage
-      .from("images")
-      .upload(fileName, myData.post_image);
-      console.log(myData,"My Data Real")
-    // if (myData.title?.contains(":"))
-    //   myData.title = myData.title.replace(":","");
-
-    if (uploadError) {
-      console.error("File upload error:", uploadError.message);
-      return;
-    }
-    const { data: publicUrlData } = supabase.storage
-      .from("images")
-      .getPublicUrl(fileName);
-
-    myData.post_image = publicUrlData.publicUrl;
 
     const { data, error } = await supabase
       .from("post")
       .insert([myData])
       .select();
-    console.log("Post created, ", data);
 
     if (data) return data[0];
 
-    // return JSON.parse(JSON.stringify(post)); // Ensure plain object serialization
   } catch (error) {
     console.error("Error creating post:", error?.message);
     return null; // Avoid returning complex objects on error
